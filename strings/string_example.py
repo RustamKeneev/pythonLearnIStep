951933c20a601c6925f9ee50ddbb53f4ef06bc98
RustamKeneev <onlineapteka.kg@gmail.com>
2022-09-10 00:28:43 +0600

    Initial commit

---
+++ strings/string_example.py
@@ -0,0 +1,26 @@
+#Типы данных строка (string - str)
+#Строки значение указывается двойным или одинарным кавычками
+"""
+#Маалымат түрлөрү сап (сап - str)
+#String мааниси кош же бир тырмакча менен көрсөтүлөт
+"""
+greeting = "Hello"
+print(greeting)
+first_name = "Rustam"
+print(first_name)
+last_name = "Keneev"
+print(last_name)
+
+spacing = " "
+
+#Мы можем сложить и сливаться с помощью знака + с конкентинацией все переменный
+"""
+#Биз + белгисин колдонуп бардык өзгөрмөлөрдү кошуп жана бириктире алабыз
+"""
+print(greeting + first_name + last_name)
+print(greeting + " " + first_name + ' ' + last_name)
+print(greeting + spacing + first_name + spacing + last_name)
+
+#Конечно мы можем в строке можеть дать значение длинную строку
+the_long_string = "This is the long string"
+print(the_long_string)
---
+++ variables/variables.py
@@ -0,0 +1,46 @@
+# variables / переменные / өзгөрмөлөр
+# стиль написания переменных (CamelCase, Kebab case, lowercase или small case)
+
+atyJonu = "Rustam" #CamelCase
+aty_jonu = "Rustam 2" #Snake Case
+
+eseptooFunctiacy = 10000
+eseptooFunctiacy22222 = 10000
+eseptoo2222222Functiacy = 10000
+_eseptooFunctiacy_ = 10000
+
+x = 50000000000
+print(type(x))
+
+x = 100000
+x = "Hello World"
+print(x)
+type_of_variable = type(x)
+print(type_of_variable)
+
+#Сумма кредита
+credit = 3000
+#Кредитная ставка
+credit_rate = 10
+#Количество лет
+number_of_years = 3
+#Рассчет
+final_summ = credit + credit / 100 * credit_rate * number_of_years
+print(final_summ)
+
+print( 1000 + 1000 / 100 * 10 * 3)
+"""
+sdjjkdhkjdsfhjkhdsjfhjkhdsfjkhdsf
+"""
+
+'''
+djdsjhdsjkhfjkdsf
+'''
+
+im = "I'm"
+print(type(im))
+print(im)
+im = 100
+print(type(im))
+im2 = 'I"m'
+print(im2)
